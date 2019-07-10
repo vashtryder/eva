ab161732cee5d2c7e99469cd376cdf92df1b7f3a
Manuel Enrique Quispe Manco <nice.forever.0321@gmail.com>
2019-07-10 17:46:02 -0500

    Init
    Base del Proyecto

---
+++ core/html/cabecera.html.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+
+<!-- 
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4 & Angular 7
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html lang="en">
+
+    <!-- begin::Head -->
+    <head>
+        <meta charset="utf-8" />
+        <title>Metronic | Dashboard</title>
+        <meta name="description" content="Latest updates and statistic charts">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!--begin::Fonts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+        <script>
+            WebFont.load({
+                google: {
+                    "families": ["Poppins:300,400,500,600,700"]
+                },
+                active: function() {
+                    sessionStorage.fonts = true;
+                }
+            });
+        </script>
+
+        <!--end::Fonts -->
+
+        <!--begin::Page Vendors Styles(used by this page) -->
+        <link href="assets/vendors/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+
+        <!--end::Page Vendors Styles -->
+
+        <!--begin:: Global Mandatory Vendors -->
+        <link href="assets/vendors/general/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" type="text/css" />
+
+        <!--end:: Global Mandatory Vendors -->
+
+        <!--begin:: Global Optional Vendors -->
+        <link href="assets/vendors/general/tether/dist/css/tether.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-datetime-picker/css/bootstrap-datetimepicker.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-timepicker/css/bootstrap-timepicker.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-select/dist/css/bootstrap-select.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/select2/dist/css/select2.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/ion-rangeslider/css/ion.rangeSlider.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/nouislider/distribute/nouislider.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/owl.carousel/dist/assets/owl.carousel.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/owl.carousel/dist/assets/owl.theme.default.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/dropzone/dist/dropzone.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/summernote/dist/summernote.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/bootstrap-markdown/css/bootstrap-markdown.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/animate.css/animate.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/toastr/build/toastr.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/morris.js/morris.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/sweetalert2/dist/sweetalert2.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/general/socicon/css/socicon.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/custom/vendors/line-awesome/css/line-awesome.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/custom/vendors/flaticon/flaticon.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/custom/vendors/flaticon2/flaticon.css" rel="stylesheet" type="text/css" />
+        <link href="assets/vendors/custom/vendors/fontawesome5/css/all.min.css" rel="stylesheet" type="text/css" />
+
+        <!--end:: Global Optional Vendors -->
+
+        <!--begin::Global Theme Styles(used by all pages) -->
+        <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+
+                <!--begin::Page Custom Styles(used by this page) -->
+        <link href="assets/app/custom/login/login-v1.default.css" rel="stylesheet" type="text/css" />
+
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+
+
+        <!--end::Global Theme Styles -->
+
+        <!--begin::Layout Skins(used by all pages) -->
+
+        <!--end::Layout Skins -->
+        <link rel="shortcut icon" href="public/sistema/logo/favicon.ico" />
+    </head>
+
+    <!-- end::Head -->
+
+
+
